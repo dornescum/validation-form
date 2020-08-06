190a071a10828eb8f947c2e6473e19fd80c679c7
Mihai Dornescu <63651677+dornescum@users.noreply.github.com>
2020-08-06 17:24:33 +0300

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PersonalContact />
+  </div>
+</template>
+<script>
+
+import PersonalContact from '../components/PersonalContact'
+
+export default {
+ components: {
+   PersonalContact
+ }
+  
+}
+</script>
+<style scoped>
+
+</style>
